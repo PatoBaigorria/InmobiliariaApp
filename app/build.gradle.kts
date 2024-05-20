53eed6f94c024a5cec5e1c6664e9e50b3d0e77f2
PatoBaigorria <mpbaigorria.01@gmail.com>
2024-05-20 16:48:51 -0300

    se agrega lib glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.play.services.maps)
     implementation(libs.retrofit)
     implementation(libs.legacy.support.v4)
+    implementation(libs.glide)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
